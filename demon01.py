c0f7131b254420078c13393ebc0ffaa779d095b2
liyuan525 <1392438744@qq.com>
2020-04-23 12:26:39 +0800

    333

--- demon01.py
+++ demon01.py
@@ -1,1 +1,67 @@
-print("我是李渊")
+import random
+
+# a=[10,20,30]
+# for val in a:
+#     print(val)
+
+
+
+# highchengji={}
+# lowchengji={}
+# student=["Lee","zhuzhu","liyuan","lili","Sunly"]
+# a=0
+# while a<len(student):
+#     chengji=int(input("请输入"+student[a]+"的成绩"))
+#     if chengji<60:
+#         lowchengji[student[a]]=chengji
+#     else:
+#         highchengji[student[a]]=chengji
+#     a=a+1
+# print(lowchengji,highchengji)
+
+
+
+# for i in range(1,10):
+#     for j in range(1,i+1):
+#         print(i,"x",j,"=",i*j,end='  ')
+#     print()
+
+
+
+
+# def yanzheng(username,password):
+#     """
+#     cdjlvnsknv;sn
+#     """
+#     if len(username)>5 and len(username)<8:
+#         if username[0] in "qwertyuiopasdfghjklzxcvbnm":
+#             if len(password)>6 and len(password)<18:
+#                 return "cdhcvhjdskvcj"
+#             else:
+#                 print("密码长度不符合")
+#         else:
+#             print("账号首字母不是小写")
+#     else:
+#         print("账号长度不合格")
+
+# a=input("请输入您的账号：")
+# b=input("请输入您的密码：")
+# aa=yanzheng(a,b)
+# print(aa)
+
+
+
+# a=input("请输入您的账号：")
+# b=input("请输入您的年龄：")
+# try:
+#     if int(b)>18:
+#         print(a,"成年了")
+#     else:
+#         print(a,"未成年")
+# except:
+#     print("请输入正确的年龄！")
+
+# a=random.randint(100,1000)
+# print(a)
+
+
---
+++ demon02.py
@@ -0,0 +1,11 @@
+import pymysql
+
+# con = pymysql.connect(host = "127.0.0.1",user = "root",password = "root",database = "tpshop")
+# cur =con.cursor()
+# cur.execute("select * from tp_goods;")
+# res=query.fetchall()
+# print(res)
+
+
+
+print("cvdjvcdksh")
